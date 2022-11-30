5a1b98a6621d7859810ff28039d61a4a16c44e6b
pranaliPukale <pukalepranali@gmail.com>
2022-11-30 00:39:06 -0800

    leftcomponent
    leftcomponent

--- typescript/react-web-app/src/App.tsx
+++ typescript/react-web-app/src/App.tsx
@@ -5,14 +5,16 @@ import ProductList from "./component/ProductList";
 import products from "./data/products.json";
 import Header1 from './component/Header1'
 import WebRoute from './WebRoute';
+import LeftComponent from './component/LeftComponent';
 
 const App=()=> {
   return (
     <div className="container-fluid">
-      <div >
+     <div>
         <Header1 />
+        <LeftComponent/>
         {/* <WebRoute/> */}
-          <ProductList data={products} cardName={"Product"} /> 
+          {/* <ProductList data={products} cardName={"Product"} />  */}
       </div>
    </div>
   );
---
+++ typescript/react-web-app/src/component/LeftComponent.tsx
@@ -0,0 +1,22 @@
+import user from "../user.png";
+const LeftComponent=()=>{
+ return(
+    <div className="row">
+      <div className="col-3 pt-3"> 
+        <h5 className='new ms-5'>*MY PROFILE*</h5> 
+        <div className="card "  >
+        <img src={user} className="card-img-top" alt="My Photo"/>
+        <div className="card-body">
+        <div className="card-text">
+           <h6><b>ABOUT ME:</b></h6> 
+          <h6><b>Name:</b>Pranali Pukale.</h6>
+          <h6><b>Course:</b>MSC</h6>   
+          <h6><b>PassOut Year:</b>2022</h6>
+        </div>
+        </div>
+        </div>
+      </div>
+    </div>
+ );
+}
+export default LeftComponent;
